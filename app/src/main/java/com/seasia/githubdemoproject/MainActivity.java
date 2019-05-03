490d61c7143541bddd8b7b2d3410da77997a3377
Jyoti1 <jyoti1@seasiainfotech.com>
2019-05-03 17:12:17 +0530

    Commit due to Commit

--- app/src/main/java/com/seasia/githubdemoproject/MainActivity.java
+++ app/src/main/java/com/seasia/githubdemoproject/MainActivity.java
@@ -7,21 +7,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnOk=findViewById(R.id.btn_click);
-        btnOk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Hello Jyoti", Toast.LENGTH_SHORT).show();
-            }
-
-
-
-        });
     }
 }
