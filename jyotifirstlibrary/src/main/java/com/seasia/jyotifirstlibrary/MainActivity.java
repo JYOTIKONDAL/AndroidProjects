1bf596feb70572be761c0cd7d17f451c10b2aca8
Jyoti1 <jyoti1@seasiainfotech.com>
2019-05-03 12:15:07 +0530

    Commit with a Module

---
+++ jyotifirstlibrary/src/main/java/com/seasia/jyotifirstlibrary/MainActivity.java
@@ -0,0 +1,24 @@
+package com.seasia.jyotifirstlibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnOk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnOk=findViewById(R.id.btn_click);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello Jyoti", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
